40ac97aa303b3d56cc97579c58edf75122be7a28
lucian <bluffyx91@gmail.com>
2023-08-12 22:50:26 +0300

    tried to use css modules; changed mind

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,29 +4,22 @@ import main from '../assets/main.svg'
 const Landing = () => {
   return (
     <div>
-      <nav className="navbar">
+      <nav>
         <img src={logo} alt="logo" />
       </nav>
-      <div
-        className="landing-page-content"
-        style={{
-          display: 'flex',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-        }}
-      >
-        <div className="landing-page-left">
-          <h1 className="landing-page-title">Job Find App</h1>
-          <p className="landing-page-text">
+      <div className="2">
+        <div className="2">
+          <h1 className="2">Job Find App</h1>
+          <p className="2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
             eveniet maxime nihil amet asperiores! Fuga consequuntur provident
             excepturi. Modi quos totam tempore ullam vitae nam ipsum architecto.
             Ea, vitae autem illo quasi, dolorum, perspiciatis commodi tempore
             neque explicabo quos nesciunt.
           </p>
-          <button className="landing-page-button btn">Login/Register</button>
+          <button className="2">Login/Register</button>
         </div>
-        <div className="landing-page-right">
+        <div className={2}>
           <img src={main} alt="main" />
         </div>
       </div>
