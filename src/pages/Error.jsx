89d0e92e088db70aaa4a993b16873b16e2c3737a
bluff <107433635+bluff91@users.noreply.github.com>
2023-09-29 15:37:17 +0300

    modified some styles in the rror page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,23 +5,37 @@ import img from '../assets/imgs/error.jpg'
 const Error = () => {
   return (
     <Wrapper>
-      <img src={img} />
-      <h3>Ohh! Page not found</h3>
-      <p>We can't seem to find the page you're looking for</p>
-      <Link to="/landing">Back Home</Link>
+      <div className='img-container'>
+        <img src={img} />
+      </div>
+      <div className="text-container">
+        <h3>Ohh! Page not found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link to="/landing">Back Home</Link>
+      </div>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
-  height: auto;
+ 
   background-color: var(--grey-200);
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  
+
+  .img-container {
+    margin-top: 20px;
+    height: 500px;
+  }
+
   img {
-    width: 1000px;
+    
+    width:100%;
+    height:100%;
+    object-fit: cover;
   }
   h3 {
     margin: 0;
